439147c44d3454b0cab0b8bbe7f19d8e3070ca40
mukesh-compiify <mukesh@compiify.com>
2025-04-22 14:58:54 +0530

    added navbar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,41 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const links = [
+    {
+        name: "home",
+        path: "/"
+    },
+    {
+        name: "services",
+        path: "/services"
+    },
+    {
+        name: "contact",
+        path: "/contact"
+    },
+    {
+        name: "resume",
+        path: "/resume"
+    },
+]
+
+
+export default function Nav() {
+    const pathName = usePathname();
+  return (
+    <nav className='flex gap-8'>
+        {
+            links.map((link,index)=>{
+                return(
+                    <Link href={link.path} key={index} className={`${link.path === pathName && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}>
+                        {link.name}
+                    </Link>
+                )
+            })
+        }
+    </nav>
+  )
+}
